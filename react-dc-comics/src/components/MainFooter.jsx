fcd7a7dda98f59b963d3ff285e6367eed87f2fba
Endri-Tosku <endritosku001@gmail.com>
2026-01-16 18:26:52 +0100

    feat: inserita qulache struttura per la seconda e terza sezione del footer con i rispettivi css di background

--- react-dc-comics/src/components/MainFooter.jsx
+++ react-dc-comics/src/components/MainFooter.jsx
@@ -1,7 +1,7 @@
 const MainFooter = () => {
     return (
         <>
-            <div className="container-footer">
+            <div className="container-footer-primo">
                 <div className="container-size-footer">
                     <ul className="ul-footer">
                         <li><a href="" className="li-footer"><img src="/buy-comics-digital-comics.png" alt="" />DIGITAL COMICS</a></li>
@@ -10,6 +10,14 @@ const MainFooter = () => {
                         <li><a href="" className="li-footer"><img src="/buy-comics-shop-locator.png" alt="" />COMIC SHOP LOCATOR</a></li>
                         <li><a href="" className="li-footer"><img src="/buy-dc-power-visa.svg" alt="" />DC POWER VISA</a></li>
                     </ul>
+                </div>
+            </div>
+            <div className="container-footer-secondo">
+                <div className="container-size-footer">
+                </div>
+            </div>
+            <div className="container-footer-terzo">
+                <div className="container-size-footer">
                 </div>
             </div>
         </>
