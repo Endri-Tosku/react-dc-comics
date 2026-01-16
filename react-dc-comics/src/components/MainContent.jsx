de08fca371ff318fccf451d9513bb7c6fc3610ac
Endri-Tosku <endritosku001@gmail.com>
2026-01-16 16:47:26 +0100

    feat: creato collegamento con il MainContent e App.jsx, inserito anche il contenuto con qualche modifica del css

---
+++ react-dc-comics/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+const MainContent = () => {
+    return (
+        <div className="container-main">
+            <div className="container-content-main">
+                <h2>-- Content goes here --</h2>
+            </div>
+        </div>
+    )
+}
+
+export default MainContent
