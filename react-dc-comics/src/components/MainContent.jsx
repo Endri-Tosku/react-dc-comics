0ebf83fefde3be15a4b4d73ee689e284be9c4960
Endri-Tosku <endritosku001@gmail.com>
2026-01-19 16:41:38 +0100

    feat: inserimento delle comics usando mapp( ) con modiche all css

--- react-dc-comics/src/components/MainContent.jsx
+++ react-dc-comics/src/components/MainContent.jsx
@@ -1,10 +1,10 @@
+import ComicsList from "./ComicsList"
+
 const MainContent = () => {
     return (
-        <div className="container-main">
-            <div className="container-content-main">
-                <h2>-- Content goes here --</h2>
-            </div>
-        </div>
+        <main>
+            <ComicsList />
+        </main>
     )
 }
 
