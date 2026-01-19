b5d965f964c8d3944aa2f83c67fac0ba894b4b9e
Endri-Tosku <endritosku001@gmail.com>
2026-01-19 17:00:44 +0100

    feat: inserita una funzione di rendering delle cards

--- react-dc-comics/src/components/ComicsList.jsx
+++ react-dc-comics/src/components/ComicsList.jsx
@@ -156,21 +156,22 @@ const ComicsList = () => {
         },
     ];
 
+    // funzione di rendering delle cards
+    const renderComicsCards = () => {
+        return comics.map((comic) => (
+            <div className="prod-card" key={comic.id}>
+                <img src={comic.thumb} />
+                <h3>{comic.title}</h3>
+            </div>
 
+        ))
+    }
 
     return (
         <div className="container-main">
             <div className="container-content-main card-container">
 
-                {comics.map((comic) => (
-                    <div className="prod-card" key={comic.id}>
-                        <img src={comic.thumb} />
-                        <h3>{comic.title}</h3>
-                    </div>
-
-                ))
-
-                }
+                {renderComicsCards()}
 
             </div>
         </div>
